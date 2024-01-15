103246a4101c7ea41c72f6beeb7165e07a16dd60
nsjames <DENIED>
2024-01-15 15:53:12 +0000

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<section>
+    <h1>Namebid fixer!</h1>
+
+    <label>The name you bid on</label><br />
+    <input type="text" placeholder="Name" bind:value={name} />
+    <br />
+    <br />
+    <button on:click={fix}>Fix</button>
+</section>
+
+<script lang="ts">
+    import { SessionKit, Chains } from "@wharfkit/session"
+    import { WebRenderer } from "@wharfkit/web-renderer"
+    import { WalletPluginAnchor } from "@wharfkit/wallet-plugin-anchor"
+
+    const webRenderer = new WebRenderer()
+
+    const sessionKit = new SessionKit({
+        appName: "appname",
+        chains: [Chains.EOS],
+        ui: webRenderer,
+        walletPlugins: [new WalletPluginAnchor()],
+    })
+
+    let name = ""
+    let bidder = ""
+
+    const fix = async () => {
+        await sessionKit.logout()
+        const { session } = await sessionKit.login()
+        const result = session.transact({
+            action: {
+                account: "eosio",
+                name: "bidrefund",
+                authorization: [session.permissionLevel],
+                data: {
+                    bidder:session.actor,
+                    newname:name,
+                },
+            },
+        })
+        console.log(result)
+    }
+</script>
